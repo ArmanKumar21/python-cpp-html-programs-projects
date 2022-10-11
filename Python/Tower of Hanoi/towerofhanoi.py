1eaaa72d29d9ad926bf81812373a0962e04a8955
Yashwanth Sai <68785131+theyashwanthsai@users.noreply.github.com>
2022-10-11 19:05:32 +0530

    Update towerofhanoi.py

--- Python/Tower of Hanoi/towerofhanoi.py
+++ Python/Tower of Hanoi/towerofhanoi.py
@@ -7,6 +7,9 @@ def tower_of_hanoi(number, source, temporary, destination):
     print("Move disk", n, "from", source, "to", destination)
     tower_of_hanoi(n-1, temporary, destination, source)
 
+# 1. Shift N-1 disks to B using C as a temporary rod.
+# 2. Shift the last disk to C.
+# 3. Now use A as a temporary rod and shift all the disks from B to C.
 
 n = int(input("Enter the number of disks"))
 tower_of_hanoi(n, 'A', 'B', 'C')
