13e41c6b47c9f525d07fd40560fdd643172436ab
Aman <paman7681@gmail.com>
2022-10-18 18:33:15 +0530

    Added Aaron's Quest IV game

---
+++ Aaron’s Quest IV While Moses Was Away/Build/config_project.js
@@ -0,0 +1,15 @@
+module.exports = function (config) {
+    config.extra_index = [{
+      name: 'crazy',
+      defines: {
+        PLATFORM: 'crazy',
+      },
+      zip: true,
+    }, {
+      name: 'itch',
+      defines: {
+        PLATFORM: 'itch',
+      },
+      zip: true,
+    }];
+  };
